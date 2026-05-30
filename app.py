8d5946c5e171cb69e88bef69d8a767f543a7a5bf
Uvyne <ropchiri7@gmail.com>
2026-05-30 14:42:01 +0200

    Allow JSON headers in CORS preflight

--- app.py
+++ app.py
@@ -86,6 +86,8 @@ def add_cors_headers(response):
     if origin in ALLOWED_ORIGINS:
         response.headers['Access-Control-Allow-Origin'] = origin
         response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
         response.headers['Vary'] = 'Origin'
     return response
 
